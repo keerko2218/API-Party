c07de027548643d3b59eae6c5d15d3c009f4ba5e
Keertana Kota <keerko2218@gmail.com>
2018-07-10 22:39:50 -0400

    Add spotify components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, NavLink, Switch } from 'react-router-dom'
+
+import './App.css'
+import Pokemon from './Pokemon'
+import Homework from './Homework'
+import Spotify from './Spotify'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/pokemon">Pok&eacute; API</NavLink>
+          </li>
+          <li>
+            <NavLink to="/homework">Homework</NavLink>
+          </li>
+          <li>
+            <NavLink to="/homework">Spotify</NavLink>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/pokemon" component={Pokemon} />
+          <Route path="/homework" component={Homework} />
+          <Route path="/spotify" component={Spotify} />
+
+          <Route render={() => (
+            <p>To get started, click one of the links above.</p>
+          )}/>
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
